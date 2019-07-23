a5082a0477adc54de9338da0b495ccbe852244c6
Mikk182 <mickael.chemin@gmail.com>
2019-07-23 17:38:40 +0200

    Update config.js

--- app/config/config.js
+++ app/config/config.js
@@ -1,7 +1,7 @@
 angular.module("config", [])
-.constant("BUCKET_SLUG", "dollfinder-app")
+.constant("COSMIC_BUCKET", "dollfinder-app")
 .constant("MEDIA_URL", "https://api.cosmicjs.com/v1/dollfinder-app/media")
 .constant("URL", "https://api.cosmicjs.com/v1/")
-.constant("READ_KEY", "aunWBeYPX4YRWNcRa92gHSwkZwJE7r4wxQbvjFpuvBTEAvYzkq")
-.constant("WRITE_KEY", "yff8Us81i1Hloe4HS1QJfsyJaiaQ0AUg9OEZhJwEWbfjPq5sUz")
+.constant("COSMIC_READ_KEY", "aunWBeYPX4YRWNcRa92gHSwkZwJE7r4wxQbvjFpuvBTEAvYzkq")
+.constant("COSMIC_WRITE_KEY", "yff8Us81i1Hloe4HS1QJfsyJaiaQ0AUg9OEZhJwEWbfjPq5sUz")
 .constant("STRIPE_KEY", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1pY2thZWwuY2hlbWluQGdtYWlsLmNvbSIsInBhc3N3b3JkIjoiIiwiaWF0IjoxNTYzODY0NDExfQ.-RCe5wqJRZZmL5fmowpy59T3M6JmgK3dy7fG4vLvv0g");
