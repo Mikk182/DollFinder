e99814a44643553f3ceffbf87c84347ee37205d7
Mikk182 <mickael.chemin@gmail.com>
2019-07-23 08:38:42 +0200

    MAJ config dollfinder-app

--- app/config/config.js
+++ app/config/config.js
@@ -1,7 +1,7 @@
 angular.module("config", [])
-.constant("BUCKET_SLUG", "ecommerce-app")
-.constant("MEDIA_URL", "https://api.cosmicjs.com/v1/ecommerce-app/media")
+.constant("BUCKET_SLUG", "dollfinder-app")
+.constant("MEDIA_URL", "https://api.cosmicjs.com/v1/dollfinder-app/media")
 .constant("URL", "https://api.cosmicjs.com/v1/")
-.constant("READ_KEY", "")
-.constant("WRITE_KEY", "")
+.constant("READ_KEY", "aunWBeYPX4YRWNcRa92gHSwkZwJE7r4wxQbvjFpuvBTEAvYzkq")
+.constant("WRITE_KEY", "yff8Us81i1Hloe4HS1QJfsyJaiaQ0AUg9OEZhJwEWbfjPq5sUz")
 .constant("STRIPE_KEY", "pk_test_oRv6WcnATRyMqponKKG6QlON");
