28ee0208fa7b2c521561bf3d0722a01856679d3b
Mikk182 <mickael.chemin@gmail.com>
2019-07-23 08:47:31 +0200

    MAJ Stripe key

--- app/config/config.js
+++ app/config/config.js
@@ -4,4 +4,4 @@ angular.module("config", [])
 .constant("URL", "https://api.cosmicjs.com/v1/")
 .constant("READ_KEY", "aunWBeYPX4YRWNcRa92gHSwkZwJE7r4wxQbvjFpuvBTEAvYzkq")
 .constant("WRITE_KEY", "yff8Us81i1Hloe4HS1QJfsyJaiaQ0AUg9OEZhJwEWbfjPq5sUz")
-.constant("STRIPE_KEY", "pk_test_oRv6WcnATRyMqponKKG6QlON");
+.constant("STRIPE_KEY", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1pY2thZWwuY2hlbWluQGdtYWlsLmNvbSIsInBhc3N3b3JkIjoiIiwiaWF0IjoxNTYzODY0NDExfQ.-RCe5wqJRZZmL5fmowpy59T3M6JmgK3dy7fG4vLvv0g");
